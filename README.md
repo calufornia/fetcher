a7d1c7bc601774da60be202b856c5d67e3a67045
Justin Lu <justinlu@berkeley.edu>
2017-04-13 03:04:27 -0700

    added requirements.txt, fixed database bug

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # fetcher
 
-curl -i -H "Content-Type: application/json" -X POST -d '{"url":"http://calufornia.github.io"}' http://localhost:5000/create
+curl -i -H "Content-Type: application/json" -X POST -d '{"URL":"http://calufornia.github.io"}' http://localhost:5000/create
 
-curl http://localhost:5000/job/
+curl -i http://localhost:5000/job/<key>
 
+PATH=$PATH:/usr/local/sbin
+~/.bash_profile
 
 rabbitmq-server
-celery worker -A fetcher.celery &
+celery worker -A fetcher.celery -n <worker>.%h &
 redis-server
--- fetcher.py
+++ fetcher.py
@@ -1,8 +1,9 @@
-import time
 from celery import Celery
+from flask import Flask, jsonify, request
 import requests
 import redis
-from flask import Flask, jsonify, request
+import pickle
+import time
 
 app = Flask(__name__)
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -11,33 +12,41 @@ celery = Celery('fetcher', backend='amqp', broker='amqp://')
 
 @app.route('/create', methods=['POST'])
 def add_job():
-    url = request.json['url']
+    url = request.json['URL']
     job = url_get.delay(url)
-    return jsonify({'key': job.task_id, 'url': url})
+    
+    return jsonify({'url': url,
+                    'key': job.task_id})
 
 
 @app.route('/job/<key>', methods=['GET'])
 def get_job(key):
-    job = celery.AsyncResult(key)
+    job = celery.AsyncResult(key) # Returns job associated with input key, None if no associated job
+    url = None
+    results = None
 
-    if job is None:
-        status = 'Not a valid key'
-    elif job.ready():
+    if job.ready():
         status = 'Done'
-        results = job.get()
-        if r.get(key) is None:
-            r.set(key, results)
+
+        if r.get(key) is None: # Performs database insertion on-demand
+            url, results = job.get()
+            r.set(key, pickle.dumps((url, results)))
+        else:
+            url, results = pickle.loads(r.get(key))
     else:
         status = 'Not Done'
 
-    return jsonify({'key': key, 'status': status, 'results': r.get(key)})
+    return jsonify({'key': key,
+                    'status': status,
+                    'url': url,
+                    'results': results})
 
 
 @celery.task
 def url_get(url):
     # time.sleep(60)  # Testing Purposes
     results = requests.get(url)
-    return results.text
+    return url, results.text
 
 
 if __name__ == '__main__':
